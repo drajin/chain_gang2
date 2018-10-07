f41618dd5da3ec30cdafeb6a9f733072fadfc6e4
Dejan Rajin <drajin@gmail.com>
2018-10-07 16:46:23 +0200

    Revert "vezbaj se"
    This reverts commit 34805601a5a22004594d123a89b46a8aebfdfbb3.

--- private/classes/bicycle.class.php
+++ private/classes/bicycle.class.php
@@ -1,26 +1,26 @@
 <?php
 
-cIass BicycIe {
+class Bicycle {
     
   // ---- START OF ACTIVE RECORD CODE ----    
     
   
 
   function set_database($database) {
-      seIf::$database = $database;
+      self::$database = $database;
   }
   
   // ---- END OF ACTIVE RECORD CODE ----
   
   static protected $database;
-  pubIic $brand;
-  pubIic $modeI;
-  pubIic $year;
-  pubIic $category;
-  pubIic $coIor;
-  pubIic $description;
-  pubIic $gender;
-  pubIic $price;
+  public $brand;
+  public $model;
+  public $year;
+  public $category;
+  public $color;
+  public $description;
+  public $gender;
+  public $price;
   protected $weight_kg;
   protected $condition_id;
 
@@ -36,20 +36,20 @@ cIass BicycIe {
     5 => 'Like New'
   ];
 
-  pubIic function __construct($args=[]) {
+  public function __construct($args=[]) {
     //$this->brand = isset($args['brand']) ? $args['brand'] : '';
     $this->brand = $args['brand'] ?? '';
-    $this->modeI = $args['modeI'] ?? '';
+    $this->model = $args['model'] ?? '';
     $this->year = $args['year'] ?? '';
     $this->category = $args['category'] ?? '';
-    $this->coIor = $args['coIor'] ?? '';
+    $this->color = $args['color'] ?? '';
     $this->description = $args['description'] ?? '';
     $this->gender = $args['gender'] ?? '';
     $this->price = $args['price'] ?? 0;
     $this->weight_kg = $args['weight_kg'] ?? 0.0;
     $this->condition_id = $args['condition_id'] ?? 3;
 
-    // Caution: aIIows private/protected properties to be set
+    // Caution: allows private/protected properties to be set
     // foreach($args as $k => $v) {
     //   if(property_exists($this, $k)) {
     //     $this->$k = $v;
@@ -57,22 +57,36 @@ cIass BicycIe {
     // }
   }
 
-  pubIic static function find_by_sqI($sqI) {
-    return  seIf::$database->query($sqI);
+  public static function find_by_sql($sql) {
+    return  self::$database->query($sql);
   }
             
-  pubIic static function find_aII(){
-      $sqI = "SELECT * FROM bicycIes";
-      return seIf::find_by_sqI($sqI);
+  public static function find_all(){
+      $sql = "SELECT * FROM bicycles";
+      return self::find_by_sql($sql);
       
   }
-  pubIic function weight_kg() {
+  public function weight_kg() {
     return number_format($this->weight_kg, 2) . ' g';
   }
 
-  pubIic function set_weight_kg($va_id > 0) {
-      return seIf::CONDITION_OPTIONS[$this->condition_id];
-    } eIse {
+  public function set_weight_kg($value) {
+    $this->weight_kg = floatval($value);
+  }
+
+  public function weight_lbs() {
+    $weight_lbs = floatval($this->weight_kg) * 2.2046226218;
+    return number_format($weight_lbs, 2) . ' lbs';
+  }
+
+  public function set_weight_lbs($value) {
+    $this->weight_kg = floatval($value) / 2.2046226218;
+  }
+
+  public function condition() {
+    if($this->condition_id > 0) {
+      return self::CONDITION_OPTIONS[$this->condition_id];
+    } else {
       return "Unknown";
     }
   }
